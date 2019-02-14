2b29919874ed00139ff25625dda5ea6f47a06da0
EZIOJQ <chenjq@umich.edu>
2019-02-14 22:55:33 +0000

    add crud

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -53,6 +53,10 @@ class AuthorListView(generic.ListView):
 class AuthorDetailView(generic.DetailView):
     model = Author
 
+class AuthorCreate(LoginRequiredMixin,CreateView):
+    model = Author
+    fields = '__all__'
+    initial = {'date_of_death': '05/01/2018'}
 
 from django.contrib.auth.mixins import LoginRequiredMixin
 
@@ -118,3 +122,23 @@ def renew_book_librarian(request, pk):
     }
 
     return render(request, 'catalog/book_renew_librarian.html', context)
+
+
+
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
+from catalog.models import Author
+
+class AuthorCreate(CreateView):
+    model = Author
+    fields = '__all__'
+    initial = {'date_of_death': '05/01/2018'}
+
+class AuthorUpdate(UpdateView):
+    model = Author
+    fields = ['first_name', 'last_name', 'date_of_birth', 'date_of_death']
+
+class AuthorDelete(DeleteView):
+    model = Author
+    success_url = reverse_lazy('authors')
